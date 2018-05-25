74f418062889baa3918b3c6130ab0e061d2547af
Eric Lamontagne <ricklamontagne@gmail.com>
2018-05-25 15:30:07 -0400

    add build commands in comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,14 @@
+//      ionic cordova build ios
+//      ionic cordova emulate ios
+//      console and livereload --> ionic cordova emulate ios -lc
+//      console and livereload --> ionic cordova emulate android -lc
+//      android w/ genyMotion --> ionic cordova run
+//      ios physical device --> ionic cordova build ios --prod
+//      debug android physical device from chrome chrome://inspect
+
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
